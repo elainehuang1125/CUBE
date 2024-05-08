1acdfb2afb7dcd137d682626fbc2fc9aec8b1025
elainehuang1125 <169027918+elainehuang1125@users.noreply.github.com>
2024-05-08 10:34:18 +0800

    Add files via upload

--- triangle.py
+++ triangle.py
@@ -1,30 +1,18 @@
 in_num = input('請輸入數字 :')
-print('請輸入數字'+ in_num )
 in_num = int(in_num)
 print('輸入數字為:', in_num )
-
 
 def triangle(num):
 	star = 1
 	for i in range(num):
-		if i == 0 :
+		if i == 0:
 			star_ = format("*"*star,"^"+str(num*2-1))
-		elif i % 2 == 0 :
-			star_ = ""
-			for j in range(num+2):
-				if j % 2 == 0 :
-					star_ = star_ + "*"
-				else :
-					star_ = star_ + " "
+		elif i == num-1:
+			star_ = format("*"+"*"*(star-2)+"*","^"+str(num*2-1))
 		else:
-			star_ = ""
-			for j in range(num+2):
-				if j % 2 != 0 :
-					star_ = star_ + "*"
-				else :
-					star_ = star_ + " "
+			star_ = format("*"+" "*(star-2)+"*","^"+str(num*2-1))
+		star += 2
 
 		print(star_)
-		
 
 triangle(in_num)
--- triangle1.py
+++ triangle1.py
@@ -1,9 +1,8 @@
 in_num = input('請輸入數字 :')
-print('請輸入數字'+ in_num )
 in_num = int(in_num)
 print('輸入數字為:', in_num )
 
-def triangle(in_num):
+def triangle(num):
 	star = 1
 	for i in range(num):
 		if i == 0:
